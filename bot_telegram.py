0b33b6998c869c497f3b03436f58be41ae40febf
josemunoz32 <joseroberto999999@gmail.com>
2025-11-07 12:27:48 -0300

    Update precio_clp calculation in bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -188,7 +188,7 @@ def procesar_mensaje(mensaje):
                 juegos.append(f"{DADO} {juego}")
         if not nickname or not juegos or not precio_usd:
             return None
-        precio_clp = precio_usd * 1000 + 15000
+        precio_clp = precio_usd * 1000 + 20000
         precio_usdt = precio_usd + 25
         mensaje_modificado = (
             f"{CASA} <b>CUENTA NINTENDO</b>\n"
